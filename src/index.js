95e63a9078cd8f69b6a919fd5c93441a0530374f
SeriousLee20 <LHuiXin01@gmail.com>
2021-07-15 00:18:01 +0800

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyInfo from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MyInfo />
   </React.StrictMode>,
   document.getElementById('root')
 );
