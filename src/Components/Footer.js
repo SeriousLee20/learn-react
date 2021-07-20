c35792b21132412ea3f0cb20f3b3d84adebc1e7d
SeriousLee20 <LHuiXin01@gmail.com>
2021-07-21 02:31:24 +0800

    Inline styline and js object

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,14 +1,29 @@
 import React from "react"
 
 function Footer(){
+    const txt = "haha"
+    const style = {
+        color: "#B72E0B",
+        backgroundColor: "orange",
+        fontSize: 12
+    }
 
+    /**
+     * jsx -> js: {}
+     * 
+     * style in jsx: accept object {{color: "yellow"}}, not string 
+     * styling in jsx: no dash in js ({} means in js), change it to camelCase, 
+     * 
+     * fontSize: default unit is px, if want specify unit, use string
+     * to change style based on condition: style.color = ""
+     */
     return (
-        
         <footer className = "note">
-            <h3>This is my footer.</h3>
+            <h3 style = {style}>This is my footer. {txt} {`${txt} ${txt}`}</h3>
         </footer>
-      
     );
+
+    // the {`${txt} ${txt}`} not '' is ``
 }
 
 export default Footer
