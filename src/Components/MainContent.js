10ef077ad584077c0b56dd0452bc1535d6dc99ae
SeriousLee20 <LHuiXin01@gmail.com>
2021-07-26 23:48:15 +0800

    Styling to-do list

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -1,10 +1,27 @@
 import React from "react"
+import TodoItem from "./TodoItem"
 
 
 function MainContent(){
 
+    //Only form elements with a name attribute will have their values passed when submitting a form.
+    //For "button", "reset", and "submit" - it defines the text on the button
+    //For "text", "password", and "hidden" - it defines the initial (default) value of the input field
+    //For "checkbox", "radio", "image" - it defines the value associated with the input (this is also the value that is sent on submit)
+    
     return (
-        <main>This is the main content.</main>
+        <div className = "mainContent">
+            <main>This is the main content.</main>
+
+            <div className = "todoList">
+
+                <TodoItem/><br/>
+                <TodoItem/><br/>
+                <TodoItem/><br/>
+            
+            </div>
+        </div>
+        
     );
 }
 
---
+++ src/Components/TodoItem.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+
+function TodoItem() {
+    return (
+    <label><input type = "checkbox" name = "checkbox1" value = " placaholder"/>haha</label>
+    );
+}
+export default TodoItem
