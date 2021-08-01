1fc107c76b1e6c3f86d9b1d2d0da93a033866673
SeriousLee20 <LHuiXin01@gmail.com>
2021-08-01 18:29:39 +0800

    Class-based Component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,16 @@
+import './App.css'
 import MyInfo from "./MyInfo";
 import Header from "./Header"
 import Footer from "./Footer";
 import MainContent from "./MainContent";
 import Contacts from "./Contacts";
-
-import './App.css'
 import Joke from "./Joke";
 import jokesData from "./jokesData";
 import products from "./vschoolProducts";
 import Product from "./Product";
+import React from "react";
 
-function App(){
+/*function App(){
 
     const jokeComponent = jokesData.map(joke => <Joke key ={joke.id} question = {joke.question} punchLine = {joke.punchLine}/>)
     const productComponent = products.map(product => <Product id = {product.id} item = {product}/>)
@@ -34,6 +34,34 @@ function App(){
             <a href="https://www.w3schools.com/tags/tag_input.asp">html input</a>
         </div>
     );
+}*/
+
+class App extends React.Component{
+    render(){
+
+        const jokeComponent = jokesData.map(joke => <Joke key ={joke.id} question = {joke.question} punchLine = {joke.punchLine}/>)
+    const productComponent = products.map(product => <Product id = {product.id} item = {product}/>)
+
+    return (
+
+        <div>
+            <Header/>
+            <MainContent/>
+            <MyInfo/>
+            <Footer/>
+            <Contacts/>
+            
+            <div>
+                {jokeComponent}
+            </div>
+
+            {productComponent}
+
+            <a href="https://www.sitepoint.com/react-components-styling-options/">css reference</a><br/>
+            <a href="https://www.w3schools.com/tags/tag_input.asp">html input</a>
+        </div>
+    );
+    }
 }
 
 export default App
