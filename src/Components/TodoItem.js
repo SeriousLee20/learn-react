30460c85fb76ce15a35b4b7f561779c887fbe279
SeriousLee20 <LHuiXin01@gmail.com>
2021-08-04 18:31:14 +0800

    set state

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -4,7 +4,11 @@ import React from "react"
 function TodoItem(props) {
     return (
     <label>
-        <input type = "checkbox" name = "checkbox" value = {props.todo.text} checked = {props.todo.completed}/>{props.todo.text}
+        <input type = "checkbox" 
+        name = "checkbox" 
+        value = {props.todo.text} 
+        checked = {props.todo.completed}/>
+        {props.todo.text}
     </label>
     );
 }
