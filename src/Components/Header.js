e758eced402a62518abe67cb11878f3c2446c72e
SeriousLee20 <LHuiXin01@gmail.com>
2021-09-07 17:19:42 +0800

    style

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component{
 
     render() {
        return (
-            <header className = "navbar">Hi {this.props.username}.</header>
+            <navbar className = "navbar">Hi {this.props.username}.</navbar>
         ); 
     }
     
