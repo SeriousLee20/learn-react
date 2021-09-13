b9bf8d211cfa54238bd6366c22598058faba75ad
SeriousLee20 <LHuiXin01@gmail.com>
2021-09-13 17:01:18 +0800

    Hook - useEffect

--- src/Components/Hook.js
+++ src/Components/Hook.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react"
-
+import React, { useEffect, useState } from "react"
+import randomColor from "randomcolor"
 
 // "hook into" state & lifacycle methods of components without using classes
 // HOOK: useState(), useEffect()
@@ -9,6 +9,9 @@ function Hook () {
     // usestate return an array when using const ***answer***
     const [answer, setAnswer] = useState("yes")
     const [count, setCount] = useState(0)
+    const [color, setColor] = useState("")
+    // if want randomColor for the first render:
+    // const[color, setColor] = useState(randomcolor())
 
     // object destructuring
     /*const person = {
@@ -27,12 +30,26 @@ function Hook () {
     }
 
 
+    // use multiple useEffect, so that the setInterval is called once only each time
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            //setCount(prevCount => prevCount + 1)
+        }, 1000)
+
+        return() => clearInterval(intervalId)
+    }, [])
+
+
+    // if empty [], render the effect for the first time then retain
+    // else, it will render effect each time the watched elements change
+    useEffect(() => {
+        setColor(randomColor())
+    }, [count])
+
     return(
         <div>
             <h1>Is state important to know? {answer}</h1>
-            <h1>{count}</h1>
-            <button onClick={increment}>Increment</button><br/>
-            <button onClick={decrement}>Decrement</button>
+            <h1 style = {{color: color}}>{count}</h1>
         </div>
     )
 }
